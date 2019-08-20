5d9892be39ad372894562cc6b4bf804e00a6cacb
Nicolas Fraisse <ni.fraisse@gmail.com>
2019-08-20 17:44:20 -0400

    authorization is back

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,11 +2,13 @@ class BookingsController < ApplicationController
   def new
     # render the view page
     @booking = Booking.new
+    authorize @lifestyle
     @lifestyle = Lifestyle.find(params[:lifestyle_id])
   end
 
   def create
     @booking = Booking.new(bookings_params)
+    authorize @lifestyle
     @booking.user = current_user
     if @booking.save
       redirect_to booking_path(@booking)
@@ -17,6 +19,7 @@ class BookingsController < ApplicationController
 
   def show
     @booking = Booking.find(params[:id])
+    authorize @lifestyle
   end
   private
   def bookings_params
