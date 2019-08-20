da5c8553bbe740ff80ab9cbf90b7f839fa2b8b08
Luc Mosser <luc.mosser86@orange.fr>
2019-08-20 14:16:15 -0400

    validations

--- app/models/lifestyle.rb
+++ app/models/lifestyle.rb
@@ -1,4 +1,8 @@
 class Lifestyle < ApplicationRecord
   has_many :bookings
   has_many :users, through: :bookings
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
 end
