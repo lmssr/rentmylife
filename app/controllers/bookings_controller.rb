cbfa4123c093134600b5e7bb672ea1b304eae9a2
Nacera HAMMID <cilacila4@gmail.com>
2019-08-20 11:37:55 -0400

    finished booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,24 @@
 class BookingsController < ApplicationController
+  def new
+    # render the view page
+    @booking = Booking.new
+  end
+
+  def create
+    @life = Lifestyle.find(params[:lifestyle_id])
+    @booking = Booking.new(bookings_params)
+    @booking.user = current_user
+    @booking.life = @life
+    if @booking.save
+      redirect_to bookings_path(@life)
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def bookings_params
+    params.require(:booking).permit(:name, :description)
+  end
 end
