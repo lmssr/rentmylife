309d16c2b8b576a55ae8a71dbf71f557efa74d2e
Nicolas Fraisse <ni.fraisse@gmail.com>
2019-08-20 16:12:12 -0400

    corrected signup issues

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+Lifestyle.destroy_all
+User.destroy_all
 nicolas = User.create(email: "ni.fraisse@gmail.com", password:"123456")
-Lifestyle.create(name: "Plumber", description: "Be Mario for a few days", price: 4)
-Lifestyle.create(name: "Dwayne Johnson", description: "Who need a description ?", price: 1000)
-Lifestyle.create(name: "Fred", description: "Play golf all day long", price: 10)
-Lifestyle.create(name: "Dog", description: "Be a dog", price: 25)
-Lifestyle.create(name: "Frog1", description: "Be a frog", price: 2895, user: nicolas)
+Lifestyle.create!(name: "Plumber", description: "Be Mario for a few days", price: 4, user: nicolas)
+Lifestyle.create!(name: "Fred", description: "Play golf all day long", price: 10, user: nicolas)
+Lifestyle.create!(name: "Dwayne Johnson", description: "Who need a description ?", price: 1000, user: nicolas)
+Lifestyle.create!(name: "Dog", description: "Be a dog", price: 25, user: nicolas)
+Lifestyle.create!(name: "Frog", description: "Be a frog", price: 2895, user: nicolas)
 
 puts "done"
