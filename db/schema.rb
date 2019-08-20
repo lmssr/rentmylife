5bfa6292dd82c8d5cc954ab38b39ab15da1e3db2
Nicolas Fraisse <ni.fraisse@gmail.com>
2019-08-20 15:41:03 -0400

    added authorizations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_19_193054) do
+ActiveRecord::Schema.define(version: 2019_08_20_192122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 2019_08_19_193054) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_lifestyles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,5 +54,6 @@ ActiveRecord::Schema.define(version: 2019_08_19_193054) do
 
   add_foreign_key "bookings", "lifestyles"
   add_foreign_key "bookings", "users"
+  add_foreign_key "lifestyles", "users"
   add_foreign_key "users", "lifestyles"
 end
