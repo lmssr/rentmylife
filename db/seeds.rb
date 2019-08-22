d715c2dbe3d107877631faa17138f2eea485aa6c
Nacera HAMMID <cilacila4@gmail.com>
2019-08-22 14:57:24 -0400

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,19 @@
 
 Booking.destroy_all
 Lifestyle.destroy_all
+Review.destroy_all
 User.destroy_all
 
+nicolas = User.create(email: "ni.fraisse@gmail.com", password:"123456", photo:"https://ichef.bbci.co.uk/news/320/cpsprodpb/1404C/production/_108369918_393c6a43-2a4f-4ef8-b113-983b552b98e2.jpg")
+# Lifestyle.create!(name: "Plumber", description: "Be Mario for a few days", price: 4, address: '799  George Street, Peterborough', user: nicolas, photo:)
+# Lifestyle.create!(name: "Fred", description: "Play golf all day long", price: 10, address: '3 avenue saint laurent, Montreal', user: nicolas,photo:)
+# Lifestyle.create!(name: "Dwayne Johnson", description: "Who need a description ?", price: 1000, address: '100 baker street', user: nicolas,photo:)
+# Lifestyle.create!(name: "Dog", description: "Be a dog", price: 25, user: nicolas,photo:)
+url = "https://ichef.bbci.co.uk/news/624/cpsprodpb/3DA1/production/_96777751_c0220207-red-eyed_treefrog-spl.jpg"
+lifestyle = Lifestyle.new(name: "Frog", description: "Be a frog", price: 2895, user: nicolas)
+lifestyle.remote_photo_url = url
+lifestyle.save
+puts "dnoe"
 
-nicolas = User.create(email: "ni.fraisse@gmail.com", password:"123456")
-Lifestyle.create!(name: "Plumber", description: "Be Mario for a few days", price: 4, address: '799  George Street, Peterborough', user: nicolas)
-Lifestyle.create!(name: "Fred", description: "Play golf all day long", price: 10, address: '3 avenue saint laurent, Montreal', user: nicolas)
-Lifestyle.create!(name: "Dwayne Johnson", description: "Who need a description ?", price: 1000, address: '100 baker street', user: nicolas)
-Lifestyle.create!(name: "Dog", description: "Be a dog", price: 25, user: nicolas)
-Lifestyle.create!(name: "Frog", description: "Be a frog", price: 2895, user: nicolas)
 
-puts "done"
+
